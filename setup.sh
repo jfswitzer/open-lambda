abef73017f631a852d371c6a8c9dd6ad44909bcc
Jennifer Switzer <jfi.switzer@gmail.com>
2023-05-04 21:41:40 +0000

    initial setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+doas apk add --update docker openrc
+doas rc-update add docker boot
+doas apk add llvm-dev
+doas apk add clang-dev
+doas apk add curl
+doas apk add alpine-sdk
+doas adduser $USER docker

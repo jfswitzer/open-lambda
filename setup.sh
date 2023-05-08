1c3de19b8cfd49a4c8c7066518064ccfdc5587df
Jennifer Switzer <jfi.switzer@gmail.com>
2023-05-07 20:56:47 -0700

    doas->sudo

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
-doas apk update
-doas apk upgrade
-doas apk add --update docker openrc
-doas rc-update add docker boot
-doas adduser $USER docker
+sudo apk update
+sudo apk upgrade
+sudo apk add --update docker openrc
+sudo rc-update add docker boot
+sudo addgroup $USER docker
 apk add --no-cache clang clang-dev alpine-sdk dpkg
 apk add --update --no-cache cmake
 apk add --update --no-cache ccache
 apk add llvm-dev
-doas apk add curl
-doas apk add go
-doas apk add gcc
+sudo apk add curl
+sudo apk add go
+sudo apk add gcc
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain=nightly-2022-07-25
 curl –proto ‘=https’ –tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env

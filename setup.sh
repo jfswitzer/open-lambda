3b17184f3567c83225c87cf4d8fc3d7a5776c3c4
Jennifer Switzer <jfi.switzer@gmail.com>
2023-05-07 20:40:25 -0700

    update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
+doas apk update
+doas apk upgrade
 doas apk add --update docker openrc
 doas rc-update add docker boot
-doas apk add llvm-dev
-doas apk add clang-dev
-doas apk add curl
-doas apk add alpine-sdk
 doas adduser $USER docker
-wget -q -O /tmp/go.tar.gz https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
-doas tar -C /usr/local -xzf /tmp/go.tar.gz
-doas ln -s /usr/local/go/bin/go /usr/bin/go
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain=nightly-2022-07-25
+apk add --no-cache clang clang-dev alpine-sdk dpkg
+apk add --update --no-cache cmake
+apk add --update --no-cache ccache
+apk add llvm-dev
+doas apk add curl
+doas apk add go
+doas apk add gcc
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain=nightly-2022-07-25
+curl –proto ‘=https’ –tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustup target add wasm32-unknown-unknown
 cargo install cross
